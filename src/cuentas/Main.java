7981382ce808056062965e5121b6e53b8f36cb65
Nairda <Nairda@DESKTOP-96OO93K>
2025-02-05 23:31:29 +0100

    Carga inicial

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -22,7 +22,7 @@ public class Main {
         double saldoActual;
         
         saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("El saldo actual es "+ saldoActual );
 
         try {
             cuenta1.retirar(2300);
